a9a92388976db403957faaf2c2c4241993249b67
permanovd <permanovd@gmail.com>
2020-02-26 19:12:07 +0000

    #1: Initial commit. Add framework files.

---
+++ src/main/java/com/permanovd/chat_api/ChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.permanovd.chat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApiApplication.class, args);
+	}
+
+}
